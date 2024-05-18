7523b4693270b494d8621c2239a402caf905940d
Mina Yousefnezhad <93423309+Minay98@users.noreply.github.com>
2024-05-18 12:40:54 +0330

    Section10

--- app/(pages)/components/Section10/components/QestionBox/index.jsx
+++ app/(pages)/components/Section10/components/QestionBox/index.jsx
@@ -1,13 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import Descripion from './components/Descripion';
+import Descripion from "./components/Descripion";
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function Index({ question,des }) {
+export default function Index({ question, des }) {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
   // ─── States ─────────────────────────────────────────────────────────────────────
@@ -25,28 +25,29 @@ export default function Index({ question,des }) {
   //
   return (
     <>
-    <section className={`${scroll}?  "grid" : "" items-center gap-6 rounded-xl border-[1px] border-[#EFEFEF] bg-white px-6 py-4`}>
       <section
-        className={` flex w-full transition-all gap-2`}
+        className={`${scroll}?  "grid" : "" items-center gap-6 rounded-xl border-[1px] border-[#EFEFEF] bg-white px-6 py-4`}
       >
-        <button
-          onClick={() => {
-            setScroll(!scroll);
-          }}
-        >
-          <Image
-            src="/images/icons/expand_24.svg"
-            width={24}
-            height={24}
-            alt=""
-          />
-        </button>
-        <h5 className="text-[16px] font-bold text-[#505050]">{question}</h5></section>
-        <section>
+        <section className={` flex w-full gap-2 transition-all`}>
+          <button
+            onClick={() => {
+              setScroll(!scroll);
+            !scroll ? document.getElementById("plus").src = '/images/icons/collapse_24.svg' : document.getElementById("plus").src = "/images/icons/expand_24.svg";
 
-        {scroll ? <Descripion des={des}  /> : ""}
-        </section></section>
-      
+            }}
+          >
+            <Image
+              src="/images/icons/expand_24.svg"
+              width={24}
+              height={24}
+              alt=""
+              id="plus"
+            />
+          </button>
+          <h5 className="text-[16px] font-bold text-[#505050]">{question}</h5>
+        </section>
+        <section>{scroll ? <Descripion des={des} /> : ""}</section>
+      </section>
     </>
   );
 }
