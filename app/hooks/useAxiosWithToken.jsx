f71e94e40021c7d8029478342c1f968edf8aff73
atefe <atefehghoddusi@gmail.com>
2024-06-26 17:04:23 +0330

    login api

--- app/hooks/useAxiosWithToken.jsx
+++ app/hooks/useAxiosWithToken.jsx
@@ -1,11 +1,13 @@
+import { NotifyMessage } from "@/enums";
+import { notify } from "@/helper";
 import axios from "axios";
-const useAxiosWithToken = axios.create({
-  headers: {
-    Authorization:
-      "bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4TVE0TEw2Z2lHQUxvcEtkZUR5UkxpdnBMeHBQM1ZuRVJ5djZ4V0ZSU0lZIn0.eyJleHAiOjE3MTU2MTY1MDEsImlhdCI6MTcxNTU4MDUwMSwianRpIjoiMmViOTAwODEtYzBiZS00MTMwLWFlNDEtY2ExNWQ0MTc2M2Q3IiwiaXNzIjoiaHR0cHM6Ly9zc28udGF6bWlueWFkYWsuY29tL2F1dGgvcmVhbG1zL3NhbmFhcCIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJiNWIwMzA3OC1hODYzLTRlZjMtYjcxMC05ZTEzMjRmODhiOGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJzYW5hYXAtcmVzdC1hcGkiLCJzZXNzaW9uX3N0YXRlIjoiYmFjNmFkMmQtODViMi00MjllLTlmZmMtNmM1ZTZkYWRkZjA1IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIiwiaHR0cHM6Ly9zc28udGF6bWlueWFkYWsuY29tIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLXNhbmFhcCIsIm9mZmxpbmVfYWNjZXNzIiwiYWRtaW4iLCJ1bWFfYXV0aG9yaXphdGlvbiIsInVzZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJzYW5hYXAtcmVzdC1hcGkiOnsicm9sZXMiOlsiY2xpZW50X3VzZXIiLCJjbGllbnRfYWRtaW4iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsInNpZCI6ImJhYzZhZDJkLTg1YjItNDI5ZS05ZmZjLTZjNWU2ZGFkZGYwNSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoidGF6bWluIn0.fPVLk4uNrKnAiBQu1TKl4q5ly1-3ihuhgOObWAFwYTXd0bQft6yIVlwJWXx0pU-TktuZ-8Vn16e4SLDeMxIVvPUla8wh9CbnjaGRztg4EdshdLffvd3oBsQDeZewPdQng5csV0cKy1kfSf-jidadkL-86AaigqTmbyUJJk0wFPryENVz5s5xi1SIdTyqfsFiGBaiseSm3VDXbmhupnMMrU0YRfe72gIeBcvtFOx5eE72DtPZAZcD3KWKIDY0QXzACwvRDRhce7rzfyQszFAs0PCT7Pwbm-V6knD2zo66TLmAZA9USCiNr6TxPJjPZcSWnPY_wlf28aqwWcV7wYp3Bg",
-  },
+const useAxiosWithToken = axios.create();
+useAxiosWithToken.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token;
+  /* config.headers.Authorization = "bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxdVMtNU9za081ZmtnX29VcDI1TXRZNlV5SUNBdVZGY1BQRW9zY251ek1ZIn0.eyJleHAiOjE3MTg1NjMxNDQsImlhdCI6MTcxODUyNzE0NCwianRpIjoiYTIzYmUxNjEtZDE3NC00ZDFlLWE5NmQtY2M2ZjRjMTEyMjQ2IiwiaXNzIjoiaHR0cHM6Ly9zc28udGF6bWlubWFzaGluLmNvbS9yZWFsbXMvc2FuYWFwIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImI3Mjg1ZjkzLTM3OTktNGZjZC1iYjExLTRlYzBhYjljMWJjMyIsInR5cCI6IkJlYXJlciIsImF6cCI6InNhbmFhcC1yZXN0LWFwaSIsInNlc3Npb25fc3RhdGUiOiI3Nzc2NmQwMi1iMWYxLTQ0Y2QtOGEyOC03YTdhZDBjYzQ0YmEiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtc2FuYWFwIiwib2ZmbGluZV9hY2Nlc3MiLCJhZG1pbiIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsic2FuYWFwLXJlc3QtYXBpIjp7InJvbGVzIjpbImNsaWVudF9hZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwic2lkIjoiNzc3NjZkMDItYjFmMS00NGNkLThhMjgtN2E3YWQwY2M0NGJhIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ0YXptaW4ifQ.L8CzD2CywziUYdhZWxG7fJPuCUnW9e_zdbidcXrvteIAOW-YAx_XKspmy1qUw2HT2L4pRmnJ9xAULZcrE1-DSlZ-qsN0WLG0mFyhqY4AJE-HO_Pj04PFOBLWHBE6d_aJe7yhgvkLTQk1EtVZLIpNKl6lKKerSSLczfF1vqyxbalsInkKo9VB4iqWqKYLag5VGx9ON8kYfnNPGtkBQQRZuN9NWtqKCTt7xJT2ZuqBbHWNVkxwFo9Ssdg80L4W-w5rFFZGRVjQPJOD4ENDgx58_n-iwh37fuIVRME0I1PAQIFBZS61F4iAiSl7Zdb2LWPvAuQpgelHN8QJx4ol5oRRdw"; */
+  return config;
 });
-
 useAxiosWithToken.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
@@ -13,14 +15,18 @@ useAxiosWithToken.interceptors.response.use(
     return response;
   },
   function (error) {
-    if (error.code === "ERR_NETWORK") {
-      notify.Error(NotifyMessage.NETWORK_ERROR)
+
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error  
+    if (error?.response?.status === 401) {
+      window.location.href = "/login";
     }
-    else {
-      // window.location.href = "/login";
-      return Promise.reject(error);
+    if (error?.response?.status === 500) {
+      notify.Error(NotifyMessage.GLOBAL_ERROR)
     }
 
+    return Promise.reject(error);
   },
 );
 export default useAxiosWithToken;
+
