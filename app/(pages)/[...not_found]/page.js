edd37cb8cf7e43944b98e682ec8e61d688469f02
ali dargahi <alidargahi1368@gmail.com>
2024-06-30 13:05:25 +0330

    .

--- app/(pages)/[...not_found]/page.js
+++ app/(pages)/[...not_found]/page.js
@@ -1,49 +1,26 @@
-"use client";
-import Section1 from "./home/Section1";
-import Section2 from "./home/Section2";
-import Section3 from "./home/Section3";
-import Section6 from "./home/Section6";
-import Section4 from "./home/Section4";
-import Section5 from "./home/Section5";
-import Section7 from "./home/Section7";
-import Section8 from "./home/Section8";
-import Section9 from "./home/Section9";
-import Section10 from "./home/Section10";
-import { UserChat } from "@/common";
-
 //
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function page() {
+export default function Index() {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
   // ─── States ─────────────────────────────────────────────────────────────────────
 
+  // ─── Functions ──────────────────────────────────────────────────────────────────
+
   // ─── Life Cycle ─────────────────────────────────────────────────────────────────
 
-  // ─── Functions ──────────────────────────────────────────────────────────────────
   //
-
   // ──────────────────────────────────────────────────── I ──────────
   //   :::::: R E N D E R : :  :   :    :     :        :          :
   // ──────────────────────────────────────────────────────────────
   //
   return (
-    <>
-      <UserChat />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
-      <Section8 />
-      <Section9 />
-      <Section10 />
-    </>
+    <section className='h-[600px] flex justify-center items-center'>
+      <h1 className='text-4xl font-bold'>صفحه مورد نظر پیدا نشد</h1>
+    </section>
   );
 }
